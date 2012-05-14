936d5912126ae71ed71b797a14221ec3bf26dcb5
fateme-norani <nfnf60@gmail.com>
2012-05-14 15:46:16 +0430

    besorate zepto

--- [php,sql]database part3/script.js
+++ [php,sql]database part3/script.js
@@ -1,13 +1,9 @@
 $(function(){
-	$('button').click(function(){
+	$('button').on('click',function(){
 		$(this).css({'background-image':'url(images/reset.jpg)'});
-		
-	});
-	
-	$('button').click(function(){
 		$('.more').slideDown();	
 	});
-	$('.more').mouseleave(function(e) {
+	$('.more').on('mouseleave',function(){
        	$(this).slideUp(600);
 		$('button').css({'background-image':'url(images/more-details-button.jpg)'});	
     });		
